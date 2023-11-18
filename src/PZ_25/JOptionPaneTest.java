55e94c7a10428cd098fd2851154736e2cf2bccd8
Student <ann.ten.tkdlove@mail.ru>
2023-11-18 15:53:22 +0300

    commit

--- src/PZ_25/JOptionPaneTest.java
+++ src/PZ_25/JOptionPaneTest.java
@@ -1,60 +1,77 @@
 package PZ_25;
 
-import javax.swing.JPanel;  //библиотека для панели приложения
-import javax.swing.JFrame;  //для создания приложения
-import java.awt.event.ActionEvent;  //для работы с действиями кнопки
-import javax.swing.JButton;  //для создания кнопок
-import javax.swing.UIManager;  //для локализации кнопок
-import javax.swing.JOptionPane;  //для диалоговых окон
-import java.awt.event.ActionListener;  //для работы с действиями кнопки
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JOptionPaneTest extends JFrame{
-    private JPanel contents = null;
-    private JButton btnMessage1 = null;
-    private JButton btnMessage2 = null;
-    private JButton btnMessage3 = null;
-
-    private JButton btnConfirm1 = null;
-    private JButton btnConfirm2 = null;
-    private JButton btnConfirm3 = null;
-
-    private JButton btnInput1 = null;
-    private JButton btnInput2 = null;
-    private JButton btnInput3 = null;
+public class JOptionPaneTest extends JFrame {
+    private JPanel contents;
+    private JButton btnGreeting;
 
     public JOptionPaneTest(){
-        super("Hello Mir");
+        super("Регистрация в программе");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        UIManager.put("OptionPane.yesButtonText", "DA");
-        UIManager.put("OptionPane.noButtonText", "NET");
-        UIManager.put("OptionPane.cancelButtonText", "OTMENA ");
+        UIManager.put("OptionPane.yesButtonText", "Да");
+        UIManager.put("OptionPane.noButtonText", "Нет");
 
         contents = new JPanel();
 
-        btnInput1 = new JButton("Hello mir");
+        btnGreeting = new JButton("Начать регистрацию");
 
-        addInputListener();
+        addListeners();
 
-        contents.add(btnInput1);
+        contents.add(btnGreeting);
 
-        //добавление панели в приложение
         setContentPane(contents);
-        setSize(600, 200);
+        setSize(400, 150);
         setVisible(true);
     }
 
-
-    private void addInputListener(){
-        btnInput1.addActionListener(new ActionListener() {
+    private void addListeners(){
+        btnGreeting.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Object result = JOptionPane.showConfirmDialog(null,
-                        "Hello. Зарегайся в программе", "Приветсвие", JOptionPane.YES_NO_OPTION);
-                JOptionPane.showConfirmDialog(null,
-                        "Hello. Зарегайся в программе", "Приветсвие", JOptionPane.YES_NO_OPTION);
+                int reply = JOptionPane.showConfirmDialog(null,
+                        "Привет! Хотите зарегистрироваться в нашей программе?",
+                            "Регистрация",
+                                JOptionPane.YES_NO_OPTION);
+                if (reply == JOptionPane.NO_OPTION) {
+                    System.exit(0);
+                } else {
+                    String username = "";
+                    while (username.length() < 5 || username.contains(" ")) {
+                        username = JOptionPane.showInputDialog(null,
+                                "Введите логин (не менее 5 символов, без пробелов):",
+                                    "Логин",
+                                        JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    String password = "";
+                    while (password.length() < 8 || password.contains(" ")) {
+                        password = JOptionPane.showInputDialog(null,
+                                "Введите пароль (не менее 8 символов, без пробелов, должен содержать цифры и буквы):",
+                                    "Пароль",
+                                        JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    String confirmPassword = JOptionPane.showInputDialog(null,
+                            "Повторите пароль:",
+                                "Подтверждение пароля",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                    while (!confirmPassword.equals(password)) {
+                        confirmPassword = JOptionPane.showInputDialog(null,
+                                "Пароли не совпадают. Повторите пароль:",
+                                    "Подтверждение пароля",
+                                        JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    JOptionPane.showMessageDialog(null,
+                            "Поздравляем, вы успешно зарегистрировались!",
+                                "Регистрация завершена",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                }
             }
         });
-
     }
 }
--- src/PZ_25/Main.java
+++ src/PZ_25/Main.java
@@ -1,11 +1,7 @@
 package PZ_25;
 
-import javax.swing.JFrame;
-
 public class Main {
-    public static void main(String[] args){
-        JFrame jFrame = new JOptionPaneTest() {}; //объект класса с пустым конструктором
-        jFrame.setVisible(true); //делаем видимым
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public static void main(String[] args) {
+        new JOptionPaneTest();
     }
 }
