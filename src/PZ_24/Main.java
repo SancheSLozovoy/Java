5714707556f85cc8038aac76fba49f9a876f6542
Student <ann.ten.tkdlove@mail.ru>
2023-11-18 13:38:41 +0300

    commit

---
+++ src/PZ_24/Main.java
@@ -0,0 +1,12 @@
+package PZ_24;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame jFrame = new JOptionPaneTest() {}; //объект класса с пустым конструктором
+        jFrame.setVisible(true); //делаем видимым
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
