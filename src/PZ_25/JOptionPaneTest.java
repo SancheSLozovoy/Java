9e333cde3ab2f1d3add9794e8cad2c794a402529
SancheSLozovoy <alex.lozovoy05@bk.ru>
2023-11-21 22:04:43 +0300

    commit

--- src/PZ_25/JOptionPaneTest.java
+++ src/PZ_25/JOptionPaneTest.java
@@ -1,6 +1,11 @@
 package PZ_25;
 
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+import javax.swing.UIManager;
+import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,10 +19,11 @@ public class JOptionPaneTest extends JFrame {
 
         UIManager.put("OptionPane.yesButtonText", "Да");
         UIManager.put("OptionPane.noButtonText", "Нет");
+        UIManager.put("OptionPane.cancelButtonText", "Отмена");
 
         contents = new JPanel();
 
-        btnGreeting = new JButton("Начать регистрацию");
+        btnGreeting = new JButton("Регистрация");
 
         addListeners();
 
@@ -25,17 +31,19 @@ public class JOptionPaneTest extends JFrame {
 
         setContentPane(contents);
         setSize(400, 150);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
     private void addListeners(){
+
         btnGreeting.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int reply = JOptionPane.showConfirmDialog(null,
                         "Привет! Хотите зарегистрироваться в нашей программе?",
-                            "Регистрация",
-                                JOptionPane.YES_NO_OPTION);
+                        "Регистрация",
+                        JOptionPane.YES_NO_OPTION);
                 if (reply == JOptionPane.NO_OPTION) {
                     System.exit(0);
                 } else {
@@ -43,35 +51,83 @@ public class JOptionPaneTest extends JFrame {
                     while (username.length() < 5 || username.contains(" ")) {
                         username = JOptionPane.showInputDialog(null,
                                 "Введите логин (не менее 5 символов, без пробелов):",
-                                    "Логин",
-                                        JOptionPane.INFORMATION_MESSAGE);
-                    }
-
-                    String password = "";
-                    while (password.length() < 8 || password.contains(" ")) {
-                        password = JOptionPane.showInputDialog(null,
-                                "Введите пароль (не менее 8 символов, без пробелов, должен содержать цифры и буквы):",
-                                    "Пароль",
-                                        JOptionPane.INFORMATION_MESSAGE);
-                    }
-
-                    String confirmPassword = JOptionPane.showInputDialog(null,
-                            "Повторите пароль:",
-                                "Подтверждение пароля",
-                                    JOptionPane.INFORMATION_MESSAGE);
-                    while (!confirmPassword.equals(password)) {
-                        confirmPassword = JOptionPane.showInputDialog(null,
-                                "Пароли не совпадают. Повторите пароль:",
-                                    "Подтверждение пароля",
-                                        JOptionPane.INFORMATION_MESSAGE);
+                                "Логин",
+                                JOptionPane.INFORMATION_MESSAGE);
                     }
 
                     JOptionPane.showMessageDialog(null,
+                            "Введите пароль (не менее 8 символов, без пробелов, должен содержать цифры и буквы):",
+                            "Ошибка",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    JPasswordField passwordField = new JPasswordField();
+                    int option = JOptionPane.showOptionDialog(null, passwordField,
+                            "Введите пароль",
+                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+                    if (option != JOptionPane.OK_OPTION) {
+                        System.exit(0);
+                    }
+                    String password = new String(passwordField.getPassword());
+                    while (password.length() < 8 || password.contains(" ") || !passwordContains(password)) {
+                        option = JOptionPane.showOptionDialog(null, passwordField,
+                                "Введите пароль (не менее 8 символов, без пробелов, должен содержать цифры и буквы):",
+                                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+
+                        if (option != JOptionPane.OK_OPTION) {
+                            System.exit(0);
+                        }
+                        password = new String(passwordField.getPassword());
+                    }
+
+
+
+                    JPasswordField confirmPasswordField = new JPasswordField();
+
+                    option = JOptionPane.showOptionDialog(null, confirmPasswordField,
+                            "Повторите пароль:",
+                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+                    if (option != JOptionPane.OK_OPTION) {
+                        System.exit(0);
+                    }
+
+                    String confirmPassword = new String(confirmPasswordField.getPassword());
+
+                    while (!confirmPassword.equals(password)) {
+                        option = JOptionPane.showOptionDialog(null, confirmPasswordField,
+                                "Пароли не совпадают. Повторите пароль:",
+                                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+
+                        if(option != JOptionPane.OK_OPTION) {
+                            System.exit(0);
+                        }
+
+                        confirmPassword = new String(confirmPasswordField.getPassword());
+                    }
+
+
+
+                    JOptionPane.showMessageDialog(null,
                             "Поздравляем, вы успешно зарегистрировались!",
-                                "Регистрация завершена",
-                                    JOptionPane.INFORMATION_MESSAGE);
+                            "Регистрация завершена",
+                            JOptionPane.INFORMATION_MESSAGE);
                 }
             }
         });
     }
+
+    public boolean passwordContains(String password){
+        boolean hasDigit =false;
+        boolean hasLetter = false;
+
+        for(char c : password.toCharArray()){
+            if(Character.isDigit(c)){
+                hasDigit = true;
+            }else if(Character.isLetter(c)){
+                hasLetter = true;
+            }
+        }
+        return hasDigit && hasLetter;
+    }
 }
+
+
+
