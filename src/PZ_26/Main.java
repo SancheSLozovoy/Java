31fd8900f78363584005a44d9300f7201dcec1c3
Student <ann.ten.tkdlove@mail.ru>
2023-11-25 17:50:21 +0300

    commit

---
+++ src/PZ_26/Main.java
@@ -0,0 +1,12 @@
+package PZ_26;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Form mainWindow = new Form();
+        mainWindow.pack();
+
+        mainWindow.setVisible(true);
+    }
+}
